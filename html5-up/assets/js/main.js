532eecb624c096131a75cc805a8555c45341146a
Anaê Soares <anae08@gmail.com>
2019-12-13 11:49:50 -0300

    subindo conteúdo de aula

---
+++ html5-up/assets/js/main.js
@@ -0,0 +1,7 @@
+//Exercício 1
+let h1 = document.getElementById("cabecalho");
+    h1.style.display = 'none';
+
+//Exercício 2
+let coruja = document.getElementById("lechuza");
+    coruja.style.filter =  'grayscale(100%)';
